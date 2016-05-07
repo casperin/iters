da390500508ea3f34aa5b2020229017d521f7071
Gorm Casper <g@g81.dk>
2016-05-07 22:48:46 +0200

    added default fn to group

--- group.js
+++ group.js
@@ -1,4 +1,6 @@
-export default function group (fn) {
+const id = x => x;
+
+export default function group (fn = id) {
   return [...this].reduce((acc, v) => {
     const key = fn(v);
     if (!acc[key]) acc[key] = [];
